1eec3e39af3c33379de4cc3e169506db37e194be
George Magdy <georgtriple@gmail.com>
2022-01-11 00:27:09 +0200

    Update homepage link and remove dummy data

--- src/App.js
+++ src/App.js
@@ -5,17 +5,10 @@ import Header from './components/Header/Header';
 import AddNewTodo from './components/Todo/AddNewTodo/AddNewTodo';
 import DisplayTodo from './components/Todo/DisplayTodo/DisplayTodo';
 
-const dummyData = [
-  { id: '1', title: 'Task 1', completed: true },
-  { id: '2', title: 'Task 2', completed: false },
-  { id: '3', title: 'Task 3', completed: false },
-  { id: '4', title: 'Task 4', completed: false },
-];
-
 const todoKey = 'todos';
 
 function App() {
-  const [todos, setTodos] = useState(dummyData);
+  const [todos, setTodos] = useState([]);
 
   useEffect(() => {
     if (localStorage.getItem(todoKey)) {
